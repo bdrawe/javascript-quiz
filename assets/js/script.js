cf68de7910e88b89e9610c0ce51052f5de64728b
B. Drawe <brycerdrawe.b@gmail.com>
2020-08-23 00:31:49 -0600

    change question functionality and also timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,28 @@
 
 // GLOBAL VARIABLES
+let timerEl  = document.getElementById('timer');
 let startBtn = document.querySelector("#start-quiz");
 let qContainer = document.querySelector("#q-container");
 let answersList = document.getElementById("answersList");
 let answerBool = document.getElementById("answer-boolean");
-let questionCounter = 0;
+let saveContainerText = document.getElementById("save-container");
 let controlButton = document.querySelector('button');
-let timeLeft = 120;
 let startButton = document.getElementById("start-quiz");
-//define score and store it in a variable
+let timeLeft = 3;
 let score = 0;
-//declare your array of objects
+let totalPts = 15;
+answerCounter = 0;
+let questionCounter = 0;
+let allPlayersScore = [];
 let questions = [
     {
        question: "When was Javascript first used?",
        answers: [
            {a: 2001, correct: false},
-           {b: 1995, correct: true},
-           {c: 1500, correct: false},
+           {a: 1995, correct: true},
+           {a: 1500, correct: false},
        ]
+       //questions[0].answers[1].correct
     },
     {
         question: "Which prompt returns the boolean true or false?",
@@ -29,59 +33,118 @@ let questions = [
         ]
      },
      {
-        question: "When will Bryce learn Javascript?",
+        question: "What is the DOM API that makes prevents the default of a form submission?",
         answers: [
-            {a: "Next Week", correct: true},
-            {a: "Today", correct: false},
-            {a: "Help", correct: false},
+            {a: ".preventDefault", correct: true},
+            {a: "_blank", correct: false},
+            {a: ".delaySubmission", correct: false},
         ]
      },
 ];
+let qOneAnswer = questions[0].answers[1].correct;
+let qTwoAnswer = questions[1].answers[0].correct;
+let qThreeAnswer = questions[2].answers[0].correct;
 
-// for(let i = 0; i < questions.length; i++){
+
 let startQuiz =  function(){
     //silence the button!
     startButton.style.display="none";
-    
+
+    for(let i = 0; i < questions.length; i++)
+    {
+        let answer = document.createElement("button");
+        answer.className = 'ansOptions';
+        answer.value = questions[i].answers[i].a;
+        answer.textContent = questions[i].answers[i].a;
+        answersList.appendChild(answer);
+        // console.log(questions[0].answers[1].correct);
+    };
     //first button
-    let answerOne = document.createElement("button");
-    answerOne.className = 'ansOptions';
-    answerOne.value = questions[0].answers[0].a;
-    answerOne.textContent = questions[0].answers[0].a;
-    answersList.appendChild(answerOne);
-    //second button
-    let answerTwo = document.createElement("button");
-    answerTwo.className = 'ansOptions';
-    answerTwo.value = questions[0].answers[1].b;
-    answerTwo.textContent = questions[0].answers[1].b;
-    answersList.appendChild(answerTwo);
-    //third button
-    let answerThree = document.createElement("button");
-    answerThree.className = 'ansOptions';
-    answerThree.value = questions[0].answers[2].c;
-    answerThree.textContent = questions[0].answers[2].c;
-    answersList.appendChild(answerThree);
-    //display the question
-    qContainer.textContent = questions[0].question;
+    // let answerOne = document.createElement("button");
+    // answerOne.className = 'ansOptions';
+    // answerOne.value = questions[0].answers[0].a;
+    // answerOne.textContent = questions[0].answers[0].a;
+    // answersList.appendChild(answerOne);
 
-    answer = document.querySelector('button');
-    answer.addEventListener("click", control);
-
+    // //second button
+    // let answerTwo = document.createElement("button");
+    // answerTwo.className = 'ansOptions';
+    // answerTwo.id = "answerTwo";
+    // answerTwo.value = questions[0].answers[1].a;
+    // answerTwo.textContent = questions[0].answers[1].a;
+    // answersList.appendChild(answerTwo);
     
+    // //third button
+    // let answerThree = document.createElement("button");
+    // answerThree.className = 'ansOptions';
+    // answerThree.value = questions[0].answers[2].a;
+    // answerThree.textContent = questions[0].answers[2].a;
+    // answersList.appendChild(answerThree);
+    // answerThree.onclick = control()
+    //display the question
+    qContainer.textContent = questions[questionCounter].question;
+
+    // answer = document.querySelector('.ansOptions');
+    answersList.addEventListener("click", control);
 };
 
-let control = function(){
-    let trueAnswer = questions[0].answers[2].b
-    if(trueAnswer === true){
+//CONTROL FUNCTION **THIS FUNCTION NEEDS HELP
+ function control(){
+     console.log("Entering Control Function");
+     console.log(event.target);
+    
+    if(null){
         alert("Yay!");
+        score+=5
     } else {
         alert("NO!")
         timeLeft -= 5;
     }
-}; 
+    questionCounter++
+    answerCounter++
+    startQuiz()
+};
+function nxtQuestion(e){
+    if(e === true){
 
+    }
+}
+
+//timer function **THIS FUNCTION IS FINE!
+function countdown(){
     
-// answer.addEventListener("click", control);
+ let timer = setInterval(function() {
+  
+    if(timeLeft > 0) {
+      timerEl.textContent = timeLeft + ' Seconds Remaining'
+      timeLeft--;
+    } else {
+      console.log("Out of time!")
+      alert("You are now out of time! Your score is " +  score + "/" + totalPts)
+      let getScore = localStorage.getItem('score');
+      clearInterval(timer);
+      highScores();
+    }
+   
+  }, 1000)
+     
+  }
+
+  //loadNewPage Function **THIS FUNCTION NEEDS HELP
+  function highScores(){
+    timerEl.style.display ="none";
+    qContainer.style.display ="none";
+    answersList.style.display ="none";
+    saveContainerText.style.display = "block";
+    //saveContainerText.textContent = score + "/" + totalPts;
+  }
+
+  function saveContainer(){
+      
+  }
+    
+
+startBtn.addEventListener("click", countdown);
 startBtn.addEventListener("click", startQuiz);
 
 
